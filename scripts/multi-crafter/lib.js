32a95a4e8cffcfa93795e5d44b52f264885fc73b
Norax <156533926+Noraxx1@users.noreply.github.com>
2024-05-17 16:49:53 +0200

    Update lib.js

--- scripts/multi-crafter/lib.js
+++ scripts/multi-crafter/lib.js
@@ -1,6 +1,7 @@
 const loader = Vars.mods.mainLoader();
 const scripts = Vars.mods.scripts;
 const NativeJavaClass = Packages.rhino.NativeJavaClass;
+Log.info("Multi crafter lib loaded");
 function getClass(name){
 	return NativeJavaClass(scripts.scope, loader.loadClass(name));
 };
