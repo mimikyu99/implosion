a19d2cc379c2066890ffe35b0a6c20158061e46a
Norax <156533926+Noraxx1@users.noreply.github.com>
2024-05-18 18:43:31 +0200

    Create update.js

---
+++ scripts/update.js
@@ -0,0 +1,51 @@
+Events.on(ClientLoadEvent, (event) => {
+    Log.info("Aquarion - Sending version check");
+    var req = new Http.get(
+        "https://raw.githubusercontent.com/0rang30rang3/implosion/mod.hjson",
+        (res) => {
+            var resp = res.getResultAsString();
+            var json = Jval.read(resp);
+            var vers = Vars.mods.getMod("aquarion").meta.version;
+
+            if (!vers.equals(json.get("version"))) {
+                try {
+                    Vars.ui.showMenu(
+                        "TEST",
+                        "TEXT GOES HERE"
+                        [["[red]Ok"], ["[green]Update"]],
+                        (option) => {
+                            if (option == 1) {
+                                Vars.ui.mods.githubImportMod(
+                                    Vars.mods.locateMod("implosion").getRepo()
+                                );
+
+                                var shown = false;
+
+                                Timer.schedule(
+                                    () => {
+                                        if (
+                                            Vars.mods.requiresReload() &&
+                                            !shown
+                                        ) {
+                                            shown = true;
+                                            Vars.ui.showInfoOnHidden(
+                                                "@mods.reloadexit",
+                                                () => {
+                                                    Core.app.exit();
+                                                }
+                                            );
+                                        }
+                                    },
+                                    2,
+                                    1
+                                );
+                            }
+                        }
+                    );
+                } catch (err) {
+                    Log.info("Error: " + err.toString());
+                }
+            }
+        },
+    );
+});
