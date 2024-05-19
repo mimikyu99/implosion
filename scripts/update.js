b65473ae3a40335e5b62ec059d3bfc109efcb796
Norax <156533926+Noraxx1@users.noreply.github.com>
2024-05-19 11:13:03 +0200

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,13 +1,16 @@
-Events.on(ClientLoadEvent.class, event -> {
-    Log.info("Implosion - Sending version check");
+//this code belongs to: https://github.com/Twcash/Tantros-Test/blob/main/scripts/update.js
 
+Events.on(ClientLoadEvent, (event) => {
+    Log.info("Implosion - Sending version check");
     var req = new Http.get(
         "https://raw.githubusercontent.com/0rang30rang3/implosion/main/mod.hjson",
-        res -> {
+        (res) => {
             var resp = res.getResultAsString();
             Log.info("Implosion - response: \n" + resp);
+
             var json = Jval.read(resp);
             Log.info("Implosion - remote ver: " + json.get("version"));
+
             var vers = Vars.mods.getMod("implosion").meta.version;
             Log.info("Implosion - local version: " + vers);
 
@@ -15,26 +18,28 @@ Events.on(ClientLoadEvent.class, event -> {
                 Log.warn("Implosion - not up to date");
                 try {
                     Vars.ui.showMenu(
-                        "\[#22CCFF\]Implosion\[white\]",
-                        Core.bundle.get("scripts.update-aquaria"),
-                        new String[][] {
-                            {"\[gray\]Ok"},
-                            {"\[green\]Reinstall"}
-                        },
-                        option -> {
+                        "[sky]Implosion[white]",
+                        Core.bundle.get("scripts.update-implosion"),
+                        [["[gray]Ok"], ["[green]Reinstall"]],
+                        (option) => {
                             if (option == 1) {
                                 Vars.ui.mods.githubImportMod(
                                     Vars.mods.locateMod("implosion").getRepo(),
                                     Vars.mods.locateMod("implosion").isJava()
                                 );
+
                                 var shown = false;
+
                                 Timer.schedule(
-                                    () -> {
-                                        if (Vars.mods.requiresReload() && !shown) {
+                                    () => {
+                                        if (
+                                            Vars.mods.requiresReload() &&
+                                            !shown
+                                        ) {
                                             shown = true;
                                             Vars.ui.showInfoOnHidden(
                                                 "@mods.reloadexit",
-                                                () -> {
+                                                () => {
                                                     Core.app.exit();
                                                 }
                                             );
@@ -46,20 +51,19 @@ Events.on(ClientLoadEvent.class, event -> {
                             }
                         }
                     );
-                } catch (Exception err) {
+                } catch (err) {
                     Log.info("Error: " + err.toString());
                 }
             } else {
                 Log.info("Implosion - up to date");
             }
         },
-        err -> {
+        (err) => {
             Vars.ui.showOkText(
-                "\[#22CCFF\]Implosion\[white\]",
-                "\[red\]ERROR:\[white\] Cannot check for updates!",
-                () -> {}
+                "[sky]Implosion[white]",
+                "[red]ERROR:[white] Cannot check for updates!",
+                () => {}
             );
             Log.err("Implosion - update check failed :(");
         }
     );
-});
