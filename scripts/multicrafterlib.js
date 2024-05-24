8a55a7f6f9f5179cdbaaecf641b64de1477a08ea
Norax <156533926+Noraxx1@users.noreply.github.com>
2024-05-24 09:23:57 +0200

    Update multicrafterlib.js

--- scripts/multicrafterlib.js
+++ scripts/multicrafterlib.js
@@ -1,33 +1,35 @@
-Log.info("MultiCrafterLib - Checking if the mod is installed");
+Events.on(ClientLoadEvent, (event) => {
+    Log.info("MultiCrafterLib - Checking if the mod is installed");
 
-var mod = Vars.mods.getMod("test");
+    var mod = Vars.mods.getMod("test");
 
-if (mod == null) {
-    Log.info("MultiCrafterLib  - not installed");
+    if (mod == null) {
+        Log.info("MultiCrafterLib - not installed");
 
-    Vars.ui.showMenu(
-        "test",
-        "test",
-        [["test"], ["test"]],
-        (option) => {
-            if (option == 1) {
-                Log.info("MultiCrafterLib - Downloading and installing the mod");
+        Vars.ui.showMenu(
+            "test",
+            "test",
+            [["test"], ["test"]],
+            (option) => {
+                if (option == 1) {
+                    Log.info("MultiCrafterLib - Downloading and installing the mod");
 
-                Vars.ui.mods.githubImportMod("liplum/MultiCrafterLib", true);
+                    Vars.ui.mods.githubImportMod("liplum/MultiCrafterLib", true);
 
-                var shown = false;
+                    var shown = false;
 
-                Timer.schedule(() => {
-                    if (Vars.mods.requiresReload() && !shown) {
-                        shown = true;
-                        Vars.ui.showInfoOnHidden("@mods.reloadexit", () => {
-                            Core.app.exit();
-                        });
-                    }
-                }, 2, 1);
+                    Timer.schedule(() => {
+                        if (Vars.mods.requiresReload() && !shown) {
+                            shown = true;
+                            Vars.ui.showInfoOnHidden("@mods.reloadexit", () => {
+                                Core.app.exit();
+                            });
+                        }
+                    }, 2, 1);
+                }
             }
-        }
-    );
-} else {
-    Log.info("TMultiCrafterLib - already installed");
-}
+        );
+    } else {
+        Log.info("MultiCrafterLib - already installed");
+    }
+});
